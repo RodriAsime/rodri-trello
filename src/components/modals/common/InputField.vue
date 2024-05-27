944dcf82ca1195966ba033b17234e44ec1a4e21a
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-05-27 20:40:12 +0200

    feat(modal): create inputs & textarea componentes

---
+++ src/components/modals/common/InputField.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineModel } from 'vue'
+defineProps({
+  ariaLabel: { type: String, required: true },
+  placeholder: { type: String, required: false, default: '' },
+  type: { type: String, required: false, default: 'text' }
+})
+
+const inputValue = defineModel({ required: true })
+</script>
+<template>
+  <input class="main-input" :type="type" v-model="inputValue" :placeholder="placeholder" />
+</template>
+
+<style scoped>
+.main-input {
+  font-size: 1.25rem;
+  margin: 0;
+  font-weight: 500;
+  border: none;
+  background-color: transparent;
+  width: 100%;
+  text-align: center;
+  position: relative;
+  border-bottom: 0.1rem solid var(--semi-transparent-dark);
+  outline: none;
+}
+</style>
---
+++ src/components/modals/common/TextAreaField.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { defineModel } from 'vue'
+defineProps({
+  ariaLabel: { type: String, required: true },
+  placeholder: { type: String, required: false, default: '' },
+  type: { type: String, required: false, default: 'text' }
+})
+
+const textareaValue = defineModel({ required: true })
+</script>
+<template>
+  <textarea
+    class="main-textarea"
+    :type="type"
+    v-model="textareaValue"
+    :placeholder="placeholder"
+  ></textarea>
+</template>
+
+<style scoped>
+.main-textarea {
+  font-size: 1.25rem;
+  margin: 0;
+  border: none;
+  background-color: transparent;
+  width: 100%;
+  text-align: center;
+  position: relative;
+  border-bottom: 0.1rem solid var(--semi-transparent-dark);
+  outline: none;
+  resize: none;
+  height: 5rem;
+}
+</style>
