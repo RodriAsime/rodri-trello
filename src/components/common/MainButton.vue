9cc7a259e777dde0557fee543fb6250e037fd96c
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-11-02 16:31:15 +0100

    feat(button): add MainButton component with styling

---
+++ src/components/common/MainButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="main-button">
+    <slot></slot>
+  </button>
+</template>
+<style scoped>
+.main-button {
+  background-color: var(--baker-miller-pink);
+  color: var(--pure-white);
+  padding: 1rem 2rem;
+  border: none;
+  outline: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  font-size: 1.2rem;
+  width: 100%;
+}
+</style>
