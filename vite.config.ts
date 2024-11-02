cfd64d8671995f8eacf67203121943140f9268de
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-11-02 16:30:53 +0100

    chore(vite): set dynamic base path for development and production

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/rodri-trello/'
+  base: process.env.NODE_ENV === 'development' ? '/' : '/rodri-trello/'
 })
